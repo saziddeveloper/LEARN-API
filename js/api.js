d5be14ce4266ddeb9d74d3af60a34086651a2a30
saziddeveloper <saziddeveloper@gmail.com>
2024-08-29 15:13:16 +0600

    all js code

--- js/api.js
+++ js/api.js
@@ -1,15 +1,38 @@
-//----------------- Comments-Container -----------------
-function loadComments(){
+function loadComments() {
     fetch("https://jsonplaceholder.typicode.com/comments")
     .then(res => res.json())
-    .then(data => displayComments(data));
+    .then(comments => {
+        comments.forEach(comment => {
+            displayComment(comment);
+        });
+    });
 }
-
-function displayComments(inputDatas){
-    const commentsContainer = document.getElementById("comments-container");
-    for (inputData of inputDatas){
-
-    }
+function displayComment(comment) {
+    fetch(`https://jsonplaceholder.typicode.com/photos/${comment.id}`)
+    .then(res => res.json())
+    .then(photo => {
+        const commentsContainer = document.getElementById("comments-container");
+        const tr = document.createElement("tr");
+        tr.innerHTML = `
+            <th class="text-center">
+                <label>${comment.id}</label>
+            </th>
+            <td>
+                <div class="flex items-center gap-3">
+                    <div class="avatar">
+                        <div class="mask mask-squircle h-12 w-12">
+                            <img src="${photo.thumbnailUrl}" alt="Comment image" />
+                        </div>
+                    </div>
+                    <div>
+                        <div class="font-bold">${comment.name}</div>
+                        <div class="text-sm opacity-50">${comment.email}</div>
+                    </div>
+                </div>
+            </td>
+            <td>${comment.body}</td>
+        `;
+        commentsContainer.appendChild(tr);
+    });
 }
-
 loadComments();
